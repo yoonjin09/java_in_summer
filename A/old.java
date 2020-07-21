93afaa8f7c23a6ddc8792a36204130371b7ea4f0
yoonjin09 <63450174+yoonjin09@users.noreply.github.com>
2020-07-21 10:31:34 +0900

    Create old.java

---
+++ A/old.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Main stepA = new Main();
+    stepA.a01();
+  }
+  void a01(){
+  	Scanner s = new Scanner(System.in);
+  	System.out.print("Enter a birth year > ");
+  	int birthyear = s.nextInt();
+  	int age = 2019-birthyear+1;
+  	System.out.println("You are "+age+" years old.");
+  }
+}
